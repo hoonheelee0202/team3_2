4e80c9eecef451c0a22af9c847f9e4c791391791
hoonheelee0202 <56126408+hoonheelee0202@users.noreply.github.com>
2019-10-08 23:45:50 -0500

    Update ipModify.sh

--- ipModify.sh
+++ ipModify.sh
@@ -4,3 +4,8 @@ my_val=$(cat resultDBServerIP | awk -F':' '{print $11}' | awk -F'"' '{print $2}'
 
 sudo sed -i 's|127.0.0.1|'$my_val'|g' /var/www/html/phpMySQLapp/books/includes/bookDatabase.php
 sudo sed -i 's|127.0.0.1|'$my_val'|g' /var/www/html/phpMySQLapp/movies/includes/movieDatabase.php
+
+sudo sed -i '$ a\[MyChameleonDB]' /etc/ansible/hosts >> bootstrap.sh
+sudo sed -i '$ a\'$my_val' ansible_connection=ssh ansible_ssh_private_key_file=~/.ssh/team3_key2.pem' /etc/ansible/hosts >> bootstrap.sh
+sudo sed -i '$ a\[MyLocalVms]' /etc/ansible/hosts >> bootstrap.sh
+sudo sed -i '$ a\127.0.0.1 ansible_connection=local' /etc/ansible/hosts >> bootstrap.sh
