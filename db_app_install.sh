4355fc24e04772834b51a1f8f4fbf0a932fe25db
hoonheelee0202 <56126408+hoonheelee0202@users.noreply.github.com>
2019-10-14 20:44:05 -0500

    Update db_app_install.sh

--- db_app_install.sh
+++ db_app_install.sh
@@ -1,4 +1,8 @@
 #!bin/bash
 
 cd
+# install mysql, git tasks, etc.
 ansible-playbook playbook_install_DBserver_applications.yml
+# create database bookdatabase, movieDB
+# populate mysqlDB and grant permission
+ansible-playbook playbook_populateDatabases.yml
